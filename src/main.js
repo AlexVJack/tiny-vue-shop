93b1074a3b176cdd03065291e9e83a9e17a1b3f1
alexvjack <alexvladjack@gmail.com>
2020-10-07 09:36:28 +0300

    upper bootstrap menu component

--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import jQuery from 'jquery'
+
+import 'popper.js'
 import 'bootstrap'
 import './assets/app.scss'
+
+window.$ = window.jQuery = jQuery
+
+Vue.component('Navbar', require('./components/Navbar.vue').default)
 
 Vue.config.productionTip = false
 
